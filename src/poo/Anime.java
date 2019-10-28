46d87d4ac852281393a502e35bc3603adf20b759
USUARIO <USUARIO@LABORATORIO-PC>
2019-10-28 09:23:40 -0300

    Inicio do projeto "Criação de Animes".

---
+++ src/poo/Anime.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+public class Anime {
+    String nome;
+    String genero;
+    String idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+    
+    
+
+    }
+   
